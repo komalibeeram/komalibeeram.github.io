1515601825bcabbadc18c0c2b16e297edcc66697
Komali Beeram <komalibeeram699@gmail.com>
2021-06-12 22:13:19 +0530

    fixed js error

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 // about section tabs
+// console.log("hi");
 (() =>{
     const aboutSection = document.querySelector(".about-section"),
     tabsContainer = document.querySelector(".about-tabs");
+    // console.log("komlai");
 
     tabsContainer.addEventListener("click", (event)=>{
         if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
+            // console.log("hello");
             const target = event.target.getAttribute("data-target");
             tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
             event.target.classList.add('active','outer-shadow');
             aboutSection.querySelector(".tab-content.active").classList.remove("active");
             aboutSection.querySelector(target).classList.add("active");
         }
-    })
-})
+    });
+})();
