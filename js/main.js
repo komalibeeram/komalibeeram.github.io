62cd48bec34daa9984f79ce3abfd628673eb1713
Komali Beeram <komalibeeram699@gmail.com>
2021-06-17 04:06:21 +0530

    mobile version

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-// about section tabs
-// console.log("hi");
-(() =>{
-    const aboutSection = document.querySelector(".about-section"),
-    tabsContainer = document.querySelector(".about-tabs");
-    // console.log("komlai");
+// menu show y hidden
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
-    tabsContainer.addEventListener("click", (event)=>{
-        if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
-            // console.log("hello");
-            const target = event.target.getAttribute("data-target");
-            tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
-            event.target.classList.add('active','outer-shadow');
-            aboutSection.querySelector(".tab-content.active").classList.remove("active");
-            aboutSection.querySelector(target).classList.add("active");
-        }
-    });
-})();
+//   menu show
+//  validate if constant exists 
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu');
+    })
+};
+//  menu hidden
+//  validate if constant exists
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    })
+};
+//  remove menu mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    // when we click on each nav__link we remove show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
